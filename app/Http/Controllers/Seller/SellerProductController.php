4e96101ade564f715477a8a69511eb5c42add22c
Jose Andres Vaca Vaca <jose.vaca@decameron.com>
2019-08-14 14:36:05 -0500

    100 Implementando el Método Store para SellerProductController

--- app/Http/Controllers/Seller/SellerProductController.php
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Seller;
 
 use App\Seller;
+use App\User;
+use App\Product;
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\ApiController;
 
@@ -19,8 +22,23 @@ class SellerProductController extends ApiController
         return $this->showAll($products);
     }
     
-    public function store(Request $request)
-    {
+    public function store(Request $request, User $seller)
+    { 
+        $rules = [
+            'name' => 'required',
+            'description' => 'required',
+            'quantity' => 'required|integer|min:1',
+            'image' =>'required|image'
+        ];
+        $this->validate($request ,$rules);
         
+        
+        $data = $request->all();
+        $data['status'] = Product::PRODUCTO_NO_DISPONIBLE;
+        $data['image'] = '1.jpg';
+        $data['seller_id'] = $seller->id;
+        
+        $product = Product::create($data);
+        return $this->showOne($product , 201);
     }
 }
