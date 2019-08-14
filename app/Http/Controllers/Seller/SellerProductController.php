97840887d9b1adae93fc3f817fe4500fa2b9fa12
Jose Andres Vaca Vaca <jose.vaca@decameron.com>
2019-08-14 14:07:47 -0500

    099 Implementando el Método Index para SellerProductController

--- app/Http/Controllers/Seller/SellerProductController.php
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -4,18 +4,23 @@ namespace App\Http\Controllers\Seller;
 
 use App\Seller;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
 
-class SellerProductController extends Controller
+class SellerProductController extends ApiController
 {
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Seller $seller)
     {
-        //
+        $products = $seller->products;
+        return $this->showAll($products);
     }
     
+    public function store(Request $request)
+    {
+        
+    }
 }
